a1986eb6db30ea27428f6662bd47c4700773ffc5
mxdc <1040558191@qq.com>
2019-07-12 14:05:42 +0800

    add static getCell from cellAdress add static getRegionCellValue add sattic getCellValueAsString

--- src/main/java/com/gzmut/office/util/ExcelUtils.java
+++ src/main/java/com/gzmut/office/util/ExcelUtils.java
@@ -1,6 +1,15 @@
 package com.gzmut.office.util;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.util.CellAddress;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
@@ -9,6 +18,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DecimalFormat;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -66,8 +77,6 @@ public class ExcelUtils {
         }
         return ;
     }
-
-
     /**
      * 判断是表名是否存在
      * @param sheetName
@@ -82,22 +91,88 @@ public class ExcelUtils {
         return  workBook.getSheet(sheetName) == null ? false : true;
     }
 
-    public static boolean checkCellValue (XSSFWorkbook workbook,String sheetName){
-
-        return  false;
-    }
-
-    public static Map<String, Map<String,String>> getCellValue(String sheetName,CellRangeAddress cellRangeAddress){
+    public static Map<Integer, Map<Integer,String>> getRegionCellValue(String sheetName,CellRangeAddress cellRangeAddress){
         // 判断workbook,sheetName,cellRangeAddress是否为空
         if (workBook == null || sheetName == null || "".equals(sheetName) || cellRangeAddress == null){
+            System.out.println("工作簿，表名或地址域为空");
             return null;
         }
         //获取表
         XSSFSheet sheet = workBook.getSheet(sheetName);
         if ( sheet == null){
+            System.out.println("表格为空");
             return null;
         }
+        // cellMap用于存放某个单元格或某个区域的值
+        Map<Integer,Map<Integer,String>> cellMap = new HashMap<Integer,Map<Integer,String>>();
+        // 遍历地址，取出单元格的值存放在map中
+        int firstRow = cellRangeAddress.getFirstRow();
+        int lastRow = cellRangeAddress.getLastRow();
+        int firstColumn = cellRangeAddress.getFirstColumn();
+        int lastColumn = cellRangeAddress.getLastColumn();
+        for(int i = firstColumn; i <= lastRow; i++){
+            Map colMap = new HashMap<Integer,String>();
+            for (int j = firstColumn; j <lastColumn ; j++) {
+                colMap.put(j,getCellValueAsString(getCell(sheet,new CellAddress(i,j))));
+            }
+            cellMap.put(i,colMap);
+        }
+        return cellMap;
+    }
 
-        return null;
+    /**
+     * 获取单元格各类型值，返回字符串类型
+     * @param cell excel 单元格
+     * @return java.lang.String
+     */
+    public static String getCellValueAsString(Cell cell){
+        // 判断是否为null或空串
+        if (cell==null || cell.toString().trim().equals("")) {
+            return "";
+        }
+        String cellValue = "";
+        // 获取单元格类型名称
+        CellType cellType = cell.getCellType();
+        switch (cellType){
+            case STRING : //字符
+                cellValue= cell.getStringCellValue().trim();
+                cellValue= StringUtils.isEmpty(cellValue) ? "" : cellValue;
+                break;
+            case BOOLEAN: //布尔值
+                cellValue = String.valueOf(cell.getBooleanCellValue());
+                break;
+            case NUMERIC: //数值,日期,货币等也为数值
+                if (HSSFDateUtil.isCellDateFormatted(cell)) {  //判断日期类型
+                    cellValue =    String.valueOf(cell.getDateCellValue());
+                } else {  //否
+                    cellValue = new DecimalFormat("#.######").format(cell.getNumericCellValue());
+                }
+                break;
+            case ERROR: // 异常
+                break;
+            case _NONE: // 未知类型
+                break;
+            case FORMULA: // 公式
+                break;
+            case BLANK: // 空白
+                break;
+            default:
+                cellValue = "";
+                break;
+        }
+        return cellValue;
+    }
+
+    /**
+     * 根据一个地址返回一个单元格
+     * @param sheet 一个excel表格
+     * @param cellAddress 一个单元格地址
+     * @return org.apache.poi.ss.usermodel.Cell
+     */
+    public static Cell getCell(XSSFSheet sheet, CellAddress cellAddress){
+        if (sheet == null || cellAddress == null){
+            return null;
+        }
+        return  sheet.getRow(cellAddress.getRow()).getCell(cellAddress.getColumn());
     }
 }
